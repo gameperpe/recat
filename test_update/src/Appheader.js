463c4b94c580e84ad0b1aa64435b37a3b5b1b51a
gameperpe <76504923+gameperpe@users.noreply.github.com>
2021-02-17 12:29:30 +0700

    recat_update
    แก้ให้สวยขึ้น

---
+++ test_update/src/Appheader.js
@@ -0,0 +1,12 @@
+function Appheader() {
+    
+    return (
+       
+          <header className="App-header">
+              <img className ="header-logo" src ="/logo192.png"/>
+    
+           
+          </header>
+          );
+          }
+    export default Appheader;
---
+++ test_update/src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"b2",
+      thumbnaiUrl :"/img/b2.png"
+      
+    
+    },
+    {
+      title :"b3",
+      thumbnaiUrl :"/img/b3.jpg"
+      
+    
+    },
+    {
+      title :"b4",
+      thumbnaiUrl :"/img/b4.jpg"
+      
+    
+    },
+    {
+      title :"b5",
+      thumbnaiUrl :"/img/b5.jpg"
+      
+    
+    }
+  ];
+  export default items;
